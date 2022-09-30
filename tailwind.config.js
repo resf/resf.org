2e37fe8fb283e71a0745b182363d7faff5397db6
Gabriel Graves <64073162+NebraskaCoder@users.noreply.github.com>
2022-09-30 02:14:30 +0000

    feat: additional tailwind config added rocky linux colors and plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          50: "#b7ead9",
+          100: "#88dcc0",
+          200: "#58cea7",
+          300: "#40c79a",
+          400: "#28c08e",
+          500: "#10b981",
+          600: "#0d9467",
+          700: "#0a6f4d",
+          800: "#064a34",
+          900: "#03251a",
+        },
+      },
+      fontFamily: {
+        display: ["Red Hat Display", "sans-serif"],
+        sans: ["Red Hat Text", "sans-serif"],
+        mono: ["Red Hat Mono", "mono"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
