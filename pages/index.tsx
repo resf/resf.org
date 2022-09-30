603eccf0b673f1a9e828643c026f07ef46699a73
Gabriel Graves <64073162+NebraskaCoder@users.noreply.github.com>
2022-09-30 02:14:48 +0000

    feat: added rocky linux green for testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,17 +13,10 @@ const Index: NextPage = () => {
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
+          <a className="text-green-600" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
           <a
