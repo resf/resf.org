477bb81b1649fcefae6367f12d316663d0ee39a3
Gabriel Graves <64073162+NebraskaCoder@users.noreply.github.com>
2022-12-02 00:50:30 +0000

    feat: extracting config to a separate file

---
+++ config/projects.ts
@@ -0,0 +1,25 @@
+export const projects = [
+  {
+    name: "Rocky Linux",
+    description: "A community-backed enterprise operating system.",
+    href: "#",
+    icon: "/rl-logo-np.svg",
+  },
+  {
+    name: "Peridot",
+    description: "Cloud-native build & release tools.",
+    href: "#",
+    icon: "/pd-logo-np.svg",
+  },
+];
+
+export const resources = [
+  {
+    name: "Charter & Bylaws",
+    href: "#",
+  },
+  {
+    name: "FAQ",
+    href: "#",
+  },
+];
