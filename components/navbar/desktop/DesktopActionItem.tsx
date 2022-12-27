59857c54872ce015b0fb8fbecad96c1a923786a4
Michael Kinder <michael@dotslash.studio>
2022-12-27 13:08:58 +0000

    chore: update target for action item in menu

--- components/navbar/desktop/DesktopActionItem.tsx
+++ components/navbar/desktop/DesktopActionItem.tsx
@@ -11,6 +11,7 @@ const DesktopActionItem = ({ children, href }: DesktopActionItemProps) => {
   return (
     <Link
       href={href}
+      target="_blank"
       className="ml-8 inline-flex items-center justify-center whitespace-nowrap border border-none bg-gradient-to-br from-green-500 to-blue-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:from-green-600 hover:to-blue-600"
     >
       {children}
