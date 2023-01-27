f26f428a14acbf48ccff9fd0ecf274e818aa586c
Michael Kinder <michael@dotslash.studio>
2023-01-27 02:19:18 +0000

    chore: remove aboutItems reference

--- components/navbar/desktop/NavbarDesktop.tsx
+++ components/navbar/desktop/NavbarDesktop.tsx
@@ -17,11 +17,7 @@ export interface NavbarDesktopProps {
   actionItems: NavActionItems;
 }
 
-const NavbarDesktop = ({
-  projectItems,
-  aboutItems,
-  actionItems,
-}: NavbarDesktopProps) => {
+const NavbarDesktop = ({ projectItems, actionItems }: NavbarDesktopProps) => {
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6">
       <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
