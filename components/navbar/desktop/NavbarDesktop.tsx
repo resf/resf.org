c9a86e7d2142366cd469a71b73f2dd22995e92a0
Michael Kinder <michael@dotslash.studio>
2023-02-16 01:34:03 +0000

    menu tweaks based on wireframes

--- components/navbar/desktop/NavbarDesktop.tsx
+++ components/navbar/desktop/NavbarDesktop.tsx
@@ -3,52 +3,48 @@ import { Popover } from "@headlessui/react";
 import DesktopItem from "./DesktopItem";
 import Logo from "./Logo";
 import MobileMenuOpenButton from "./MobileMenuOpenButton";
-import DesktopDropdownItem from "./DesktopDropdownItem";
-import DesktopActionItem from "./DesktopActionItem";
+// import DesktopDropdownItem from "./DesktopDropdownItem";
+// import DesktopActionItem from "./DesktopActionItem";
 
 import type { NavItems } from "@/types/navigation/NavItem";
-import type { NavActionItems } from "@/types/navigation/NavActionItem";
+// import type { NavActionItems } from "@/types/navigation/NavActionItem";
 
 export interface NavbarDesktopProps {
-  projectItems: NavItems;
+  // projectItems: NavItems;
   menuItems: NavItems;
-  actionItems: NavActionItems;
+  // actionItems: NavActionItems;
 }
 
 const NavbarDesktop = ({
-  projectItems,
+  // projectItems,
   menuItems,
-  actionItems,
-}: NavbarDesktopProps) => {
+}: // actionItems,
+NavbarDesktopProps) => {
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6">
-      <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-        <div className="flex justify-start lg:w-0 lg:flex-1">
+    <div className="mx-auto max-w-7xl sm:px-2">
+      <div className="flex items-center justify-between p-6 md:justify-start md:space-x-10">
+        <div>
           <Logo />
         </div>
         <div className="-my-2 -mr-2 md:hidden">
           <MobileMenuOpenButton />
         </div>
-        <Popover.Group as="nav" className="hidden space-x-10 md:flex">
-          {menuItems.map((item) => (
-            <DesktopItem key={item.name} href={item.href}>
-              {item.name}
-            </DesktopItem>
-          ))}
-          <DesktopDropdownItem
-            items={projectItems}
-            openNewTab={true}
-            isFirstMenuItem={true}
-          >
-            Projects
-          </DesktopDropdownItem>
-        </Popover.Group>
-        <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-          {actionItems.map((item) => (
-            <DesktopActionItem key={item.name} href={item.href}>
-              {item.name}
-            </DesktopActionItem>
-          ))}
+        <div className="hidden md:flex md:flex-1 md:items-center md:justify-between">
+          <Popover.Group as="nav" className="flex space-x-10">
+            {menuItems.map((item) => (
+              <DesktopItem key={item.name} href={item.href}>
+                {item.name}
+              </DesktopItem>
+            ))}
+            {/* Leaving this functionality in case we need it in the future. */}
+            {/* <DesktopDropdownItem
+              items={projectItems}
+              openNewTab={true}
+              isFirstMenuItem={true}
+            >
+              Projects
+            </DesktopDropdownItem> */}
+          </Popover.Group>
         </div>
       </div>
     </div>
