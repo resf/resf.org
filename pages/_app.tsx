6b945e44752bba36984b5f5dd882bac85d579791
Michael Kinder <me@mwk.email>
2023-02-27 12:06:17 -0500

    add plausible script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import { appWithTranslation } from "next-i18next";
+import PlausibleProvider from "next-plausible";
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <PlausibleProvider domain="resf.org">
+        <Component {...pageProps} />
+      </PlausibleProvider>
+  );
 }
 
 export default appWithTranslation(MyApp);
