a91db729e70802f713340115aec635b0193b2bb5
Michael Kinder <me@mwk.email>
2023-03-08 00:08:39 +0000

    add proxied url to plausible

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
+const { withPlausibleProxy } = require("next-plausible");
 
-const nextConfig = {
+module.exports = withPlausibleProxy({
+  customDomain: "https://img.resf.workers.dev",
+})({
   reactStrictMode: true,
   swcMinify: true,
   i18n,
-};
-
-module.exports = nextConfig;
+});
