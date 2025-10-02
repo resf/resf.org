ec2f02fba8303542e76be42436a9bed1ab984024
Brian Clemens <brian@tiuxo.com>
2025-10-02 14:46:41 +0900

    Add previous directors section, social links, updates

--- config/board.ts
+++ config/board.ts
@@ -3,67 +3,94 @@ export const board = [
 		name: "Gregory Kurtzer",
 		image: "/gregk.jpeg",
 		role: "President, Chair",
+		active: true,
 		socials: {
 			linkedIn: "https://www.linkedin.com/in/gmkurtzer/",
+			bluesky: "https://bsky.app/profile/gmkurtzer.bsky.social",
 		},
 	},
 	{
 		name: "Brian Clemens",
-		image: "/brianc.jpg",
+		image: "/brianc.png",
 		role: "Vice President",
+		active: true,
 		socials: {
-			linkedIn: "https://tiuxo.com",
+			website: "https://tiuxo.com",
+			bluesky: "https://bsky.app/profile/tiuxo.com",
+			mastodon: "https://gts.tiuxo.com/@brian",
 		},
 	},
 	{
 		name: "Louis Abel",
 		image: "/louisa.jpg",
 		role: "Vice Chair",
+		active: true,
 		socials: {
 			linkedIn: "https://www.linkedin.com/in/louis-abel/",
+			bluesky: "https://bsky.app/profile/angelsofclockwork.net",
 		},
 	},
 	{
 		name: "Christopher Stackpole",
 		image: "/christophers.jpg",
 		role: "Board Member",
+		active: true,
 	},
 	{
 		name: "Mustafa Gezen",
 		image: "/mustafag.jpg",
 		role: "Board Member",
+		active: true,
 		socials: {
 			linkedIn: "https://www.linkedin.com/in/mustafa-gezen/",
+			bluesky: "https://bsky.app/profile/mstg.bsky.social",
 		},
 	},
 	{
 		name: "Neil Hanlon",
 		image: "/neilh.png",
 		role: "Board Member",
+		active: true,
 		socials: {
+			website: "https://shrug.pw",
 			linkedIn: "https://www.linkedin.com/in/hanlonneil/",
+			bluesky: "https://bsky.app/profile/shrug.pw",
 		}
 	},
 	{
 		name: "Sherif Nagy",
 		image: "/sherifn.jpeg",
 		role: "Board Member",
+		active: true,
 		socials: {
 			linkedIn: "https://www.linkedin.com/in/sherifn/",
+			bluesky: "https://bsky.app/profile/gnulab.org",
 		},
 	},
 	{
 		name: "Taylor Goodwill",
 		image: "/taylorg.jpg",
 		role: "Board Member",
+		active: true,
 		socials: {
 			linkedIn: "https://www.linkedin.com/in/taylorgoodwill/",
+			bluesky: "https://bsky.app/profile/limin.nl",
+		},
+	},
+	{
+		name: "Buyanjargal Shirnen",
+		image: "/buyas.jpg",
+		role: "Independent",
+		active: true,
+		socials: {
+			linkedIn: "https://www.linkedin.com/in/buyanjargal-shirnen/",
 		},
 	},
 	{
 		name: "Mark Watson",
 		image: "/markw.png",
 		role: "Independent",
+		active: false,
 		socials: {
 			linkedIn: "https://www.linkedin.com/in/mark-a-watson-9035b59/",
 		},
@@ -72,14 +99,17 @@ export const board = [
 		name: "Christopher DiBona",
 		image: "/christopherd.jpg",
 		role: "Independent",
+		active: true,
 		socials: {
 			linkedIn: "https://www.linkedin.com/in/cdibona/",
+			bluesky: "https://bsky.app/profile/cdibona.bsky.social",
 		},
 	},
 	{
 		name: "Benjamin Agner",
 		image: "/benjamina.jpg",
 		role: "Independent",
+		active: true,
 		socials: {
 			linkedIn: "https://www.linkedin.com/in/benjaminagner/",
 		},
@@ -88,8 +118,20 @@ export const board = [
 		name: "Theodore Ts'o",
 		image: "/theodoret.jpg",
 		role: "Independent",
+		active: true,
 		socials: {
 			linkedIn: "https://www.linkedin.com/in/tytso/",
+		},
+	},
+	{
+		name: "Greg Kroah-Hartman",
+		image: "/gregkh.jpg",
+		role: "Independent",
+		active: false,
+		socials: {
+			website: "https://www.kroah.com/log",
+			mastodon: "https://social.kernel.org/users/gregkh",
+			linkedIn: "https://www.linkedin.com/in/greg-kroah-hartman/",
 		},
 	},
 ];
